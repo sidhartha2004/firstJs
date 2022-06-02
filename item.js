18c0e67802ba365887b4c8e10edf0765ef27f11e
sidhartha2004 <106370287+sidhartha2004@users.noreply.github.com>
2022-06-02 22:42:05 +0530

    itemList

---
+++ item.js
@@ -0,0 +1,10 @@
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textcontent= 'Hello2';
+items[0].style.fontWeight='Bold';
+items[1].style.fontWeight='Bold';
+items[2].style.fontWeight='Bold';
+items[3].style.fontWeight='Bold';
+items[1].style.backgroundColor = 'Yellow';
+items[2].style.backgroundColor = 'green';
